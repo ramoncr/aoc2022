9c457ce6b88247598e8f41a3984a56fee7ddecdf
Ramon <ramoncr@users.noreply.github.com>
2022-12-01 16:59:06 +0100

    Cleanup

--- AoC.One/Program.cs
+++ AoC.One/Program.cs
@@ -1,49 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AoC.One;
+using Microsoft.Extensions.Logging;
+using Utilities;
 
-Console.WriteLine("Hello, World!");
+var logger = LogFactory.GetLogger();
+logger.LogInformation("Let's count some calories for the elves.");
 
 var provisionList = Provisions.GetList();
-
-
 var elfIndex = 0;
 var currentCalories = 0;
-
 var caloriesPerElf = new Dictionary<int, int>();
-
-
-var highestAmountOfCalories = 0;
 
 foreach (var rawCalories in provisionList)
 {
     if (string.IsNullOrWhiteSpace(rawCalories))
     {
-        if (currentCalories > highestAmountOfCalories) highestAmountOfCalories = currentCalories;
-
         caloriesPerElf.Add(elfIndex, currentCalories);
-
         elfIndex++;
         currentCalories = 0;
         continue;
     }
 
-    var calories = int.Parse(rawCalories);
-    currentCalories += calories;
-
+    currentCalories += int.Parse(rawCalories);
 }
 
-Console.WriteLine($"Hightest Calories: {highestAmountOfCalories}");
+var top3 = caloriesPerElf.OrderByDescending(kvp => kvp.Value).Take(3).ToList();
 
-;
+logger.LogInformation("Top 3 elves with most calories:");
+for (int i = 0; i < top3.Count; i++)
+    logger.LogInformation("{position:00}. Elf no. {elfIndex} with {calories}", i, top3[i].Key, top3[i].Value);
 
-var top3 = caloriesPerElf.OrderByDescending(kvp => kvp.Value).Take(3);
-Console.WriteLine("Top 3:");
-foreach (var kvp in
-top3)
-{
-    Console.WriteLine($"{kvp.Key}: {kvp.Value}");
-}
-
-
-Console.WriteLine($"Total: {top3.Select(kvp => kvp.Value).Sum()}");
-
+logger.LogInformation("With a total of {top3TotalCalories}", top3.Select(kvp => kvp.Value).Sum());
